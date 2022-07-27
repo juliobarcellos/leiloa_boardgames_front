e3c90b349b71a5bc363f007fef1bee977baedb96
Julio Barcellos <46910743+juliobarcellos@users.noreply.github.com>
2022-07-27 01:26:51 -0300

    Inserindo rotas teste para os modais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginModalWrapper from './components/Modals/Login/LoginModalWrapper';
+import RegisterModalWrapper from './components/Modals/Register/RegisterModalWrapper';
+import ForgotPasswordWrapper from './components/Modals/ForgotPassword/ForgotPasswordWrapper';
 import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route path="/login" element={<LoginModalWrapper />} />
+            <Route path="/register" element={<RegisterModalWrapper />} />
+            <Route path="/forgot_password" element={<ForgotPasswordWrapper />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
