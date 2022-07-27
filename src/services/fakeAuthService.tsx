fdbb6f70e3b83e72950a7758548fed694c1e0e1f
Julio Barcellos <46910743+juliobarcellos@users.noreply.github.com>
2022-07-27 01:28:43 -0300

    adicionando serviço simples de login

---
+++ src/services/fakeAuthService.tsx
@@ -0,0 +1,31 @@
+
+import { AuthArgs } from "../types"
+
+
+const loginUser = ({ password, login }: AuthArgs): Promise<boolean> => new Promise((resolve, reject) => setTimeout(() => {
+  if (login === 'CyberPotato' && password === 'test1234') {
+    resolve(true)
+  } else {
+    reject('Credentials are wrong')
+  }
+}, 1500))
+
+const registerUser = ({ password, login }: AuthArgs): Promise<boolean> => new Promise((resolve, reject) => setTimeout(() => {
+  if (login === 'CyberPotato') {
+    reject('Login already taken')
+  } else {
+    resolve(true)
+  }
+}, 1500))
+
+const forgotPassword = (login: string): Promise<boolean> => new Promise((resolve, reject) => setTimeout(() => {
+  if (login === 'CyberPotato') {
+    resolve(true);
+  }
+}, 1500))
+
+export {
+  loginUser,
+  registerUser,
+  forgotPassword
+}
