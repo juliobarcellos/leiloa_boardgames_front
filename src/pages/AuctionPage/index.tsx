f1e958f5a13b98223d0a63a9d583a369a7cd77ba
Julio Barcellos <julio.barcellos@groove.tech>
2022-08-15 21:21:47 -0300

    Inserindo a página de detalhes de um leilão

--- src/pages/AuctionPage/index.tsx
+++ src/pages/AuctionPage/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TimeLeftBox from "../../components/TimeLeftBox";
 import TopBarMenu from "../../components/TopBarMenu";
 import ImageGallery from 'react-image-gallery';
+import { BsTruck } from 'react-icons/bs';
 import './AuctionPage.scss';
 
 
@@ -10,21 +11,32 @@ export default function AuctionPage() {
     const images = [
         {
             original:
-                'https://www.residentevildatabase.com/wp-content/uploads/2017/09/re2-board-game-0.jpg',
+                'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg',
             originalWidth: 480,
             originalHeight: 480,
             thumbnail:
-                'https://www.residentevildatabase.com/wp-content/uploads/2017/09/re2-board-game-0.jpg',
+                'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg',
             thumbnailWidth: 80,
             thumbnailHeight: 80,
         },
         {
             original:
-                'https://i.pinimg.com/originals/7f/22/1e/7f221e2f7074c5b1050c78ea4bfdb8b8.jpg',
+                'https://www.mundogalapagos.com.br/ccstore/v1/images/?source=/file/v2254333255858590756/products/RSU001_03.jpg',
             originalWidth: 480,
             originalHeight: 480,
             thumbnail:
-                'https://i.pinimg.com/originals/7f/22/1e/7f221e2f7074c5b1050c78ea4bfdb8b8.jpg',
+                'https://www.mundogalapagos.com.br/ccstore/v1/images/?source=/file/v2254333255858590756/products/RSU001_03.jpg',
+            thumbnailWidth: 80,
+            thumbnailHeight: 80,
+        },
+
+        {
+            original:
+                'https://cdn.awsli.com.br/300x300/177/177316/produto/59982522/f6951ec8ff.jpg',
+            originalWidth: 480,
+            originalHeight: 480,
+            thumbnail:
+                'https://cdn.awsli.com.br/300x300/177/177316/produto/59982522/f6951ec8ff.jpg',
             thumbnailWidth: 80,
             thumbnailHeight: 80,
         },
@@ -42,7 +54,7 @@ export default function AuctionPage() {
                     <span className="dados__subtitulo">Detalhes do jogo que o vendedor acredita necessitar de destaque</span>
                     <div className="dados__flexContainer">
                         <div className="flexContainer__selecionarLance">
-                            <h4>Selecione o seu lance</h4>
+                            <h4 className="selecionarLance__title">Selecione o seu lance</h4>
                             <select className="selecionarLance__select">
                                 <option>R$500,00</option>
                                 <option>R$510,00</option>
@@ -60,44 +72,53 @@ export default function AuctionPage() {
                         </div>
                     </div>
                     <div className="dados__flexContainer">
-                        <div className="flexContainer__SimularFrete">
-                            <span>Simular Frete:</span>
-                            <input placeholder="12345-678" />
-                            <button>C</button>
-                        </div>
+                        <button className="dados__botaoDarLance">Dar Lance!</button>
                         <div className="flexContainer__leiloeiro">
-                            <img src="" alt="" />
+                            <div className="leiloeiro__box">
+                                <img className="leiloeiro__image" src="https://static-cse.canva.com/blob/892615/1600w-EW4cggXkgbc.jpg" alt="" />
+                                <span className="leiloeiro_link">Ver Perfil</span>
+                            </div>
                             <div className="leiloeiro__textos">
                                 <span>Leiloeiro(a): Matilda Brown</span>
                                 <span>٭٭٭٭٭(10 avaliações)</span>
                             </div>
-                            <span>Ver Perfil</span>
                         </div>
                     </div>
-                    <button>Dar Lance!</button>
+
+                    <div className="flexContainer__SimularFrete">
+                            <span>Simular Frete:</span>
+                            <div className="simularFrete__inputButton">
+                                <input placeholder="12345-678" />
+                                <button>
+                                    <BsTruck size={20} />
+                                </button>
+                            </div>
+                        </div>
                 </div>
 
             </div>
             <div className="DetalhesProduto">
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis mi ac consectetur mollis. Aliquam faucibus venenatis ornare. Curabitur feugiat facilisis lorem. Nullam tincidunt felis sit amet tincidunt dictum. Ut at risus ullamcorper, ornare augue laoreet, congue ligula. Mauris id ornare sem, et imperdiet tortor. Suspendisse rutrum et magna non vestibulum. Aenean posuere, erat vel posuere imperdiet, urna orci pretium sem, eu eleifend risus augue ac ligula. Mauris enim ligula, tempus sit amet velit nec, aliquet pharetra neque. Nam venenatis nunc scelerisque, feugiat orci at, bibendum eros. Suspendisse venenatis purus id dui lacinia sodales. Nam ligula eros, faucibus et viverra in, ultricies vel nulla. Sed nec velit nunc. Praesent in dignissim mauris. Proin semper quam ut dolor placerat condimentum.</span>
+                <span className="DetalhesProduto__title">Detalhes do produto</span>
+                <span className="DetalhesProduto__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis mi ac consectetur mollis. Aliquam faucibus venenatis ornare. Curabitur feugiat facilisis lorem. Nullam tincidunt felis sit amet tincidunt dictum. Ut at risus ullamcorper, ornare augue laoreet, congue ligula. Mauris id ornare sem, et imperdiet tortor. Suspendisse rutrum et magna non vestibulum. Aenean posuere, erat vel posuere imperdiet, urna orci pretium sem, eu eleifend risus augue ac ligula. Mauris enim ligula, tempus sit amet velit nec, aliquet pharetra neque. Nam venenatis nunc scelerisque, feugiat orci at, bibendum eros. Suspendisse venenatis purus id dui lacinia sodales. Nam ligula eros, faucibus et viverra in, ultricies vel nulla. Sed nec velit nunc. Praesent in dignissim mauris. Proin semper quam ut dolor placerat condimentum.</span>
             </div>
             <div className="FichaTecnica">
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis mi ac consectetur mollis. Aliquam faucibus venenatis ornare. Curabitur feugiat facilisis lorem. Nullam tincidunt felis sit amet tincidunt dictum. Ut at risus ullamcorper, ornare augue laoreet, congue ligula. Mauris id ornare sem, et imperdiet tortor. Suspendisse rutrum et magna non vestibulum. Aenean posuere, erat vel posuere imperdiet, urna orci pretium sem, eu eleifend risus augue ac ligula. Mauris enim ligula, tempus sit amet velit nec, aliquet pharetra neque. Nam venenatis nunc scelerisque, feugiat orci at, bibendum eros. Suspendisse venenatis purus id dui lacinia sodales. Nam ligula eros, faucibus et viverra in, ultricies vel nulla. Sed nec velit nunc. Praesent in dignissim mauris. Proin semper quam ut dolor placerat condimentum.</span>
+                <span className="FichaTecnica__title">Ficha Técnica</span>
+                <span className="FichaTecnica__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis mi ac consectetur mollis. Aliquam faucibus venenatis ornare. Curabitur feugiat facilisis lorem. Nullam tincidunt felis sit amet tincidunt dictum. Ut at risus ullamcorper, ornare augue laoreet, congue ligula. Mauris id ornare sem, et imperdiet tortor. Suspendisse rutrum et magna non vestibulum. Aenean posuere, erat vel posuere imperdiet, urna orci pretium sem, eu eleifend risus augue ac ligula. Mauris enim ligula, tempus sit amet velit nec, aliquet pharetra neque. Nam venenatis nunc scelerisque, feugiat orci at, bibendum eros. Suspendisse venenatis purus id dui lacinia sodales. Nam ligula eros, faucibus et viverra in, ultricies vel nulla. Sed nec velit nunc. Praesent in dignissim mauris. Proin semper quam ut dolor placerat condimentum.</span>
             </div>
             <div className="Perguntas">
-                <h3>Tem uma pergunta?</h3>
-                <textarea />
-                <h4>12 Perguntas</h4>
-                <div className="pergunta">
-                    <div>
-                        <img src="" alt="" />
-                        <span>Responder</span>
+                <h3 className="Perguntas__title">Tem uma pergunta?</h3>
+                <textarea className="Perguntas__text" placeholder='Escreva aqui a sua pergunta e tecle "Enter" para enviar' />
+                <h4 className="Perguntas__qtd">12 Perguntas</h4>
+                <div className="Perguntas__pergunta">
+                    <div className="pergunta__imgBox">
+                        <img className="pergunta__img" src="https://static-cse.canva.com/blob/892615/1600w-EW4cggXkgbc.jpg" alt="" />
+                        <span className="pergunta__button">Responder</span>
                     </div>
-                    <div>
-                        <span>Helene Moore</span>
-                        <span>The dress is great! Very classy and comfortable. It fit perfectly! I'm 5'7" and 130 pounds. I am a 34B chest. This dress would be too long for those who are shorter but could be hemmed. I wouldn't recommend it for those big chested as I am smaller chested and it fit me perfectly. The underarms were not too wide and the dress was made well.</span>
+                    <div className="pergunta__box">
+                        <span className="pergunta__user">Helene Moore</span>
+                        <span className="pergunta__text">The dress is great! Very classy and comfortable. It fit perfectly! I'm 5'7" and 130 pounds. I am a 34B chest. This dress would be too long for those who are shorter but could be hemmed. I wouldn't recommend it for those big chested as I am smaller chested and it fit me perfectly. The underarms were not too wide and the dress was made well.</span>
                     </div>
-                    <span>5 de set. de 2022</span>
+                    <span className="pergunta__date">5 de set. de 2022</span>
                 </div>
             </div>
         </>
