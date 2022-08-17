cca72997c9d9b1180f7ff8b57f1bcb4c4acedc36
Julio Barcellos <julio.barcellos@groove.tech>
2022-08-17 01:45:48 -0300

    Incluíndo rotina scroll to top ao usar react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import RegisterModalWrapper from './components/Modals/Register/RegisterModalWrap
 import ForgotPasswordWrapper from './components/Modals/ForgotPassword/ForgotPasswordWrapper';
 import Home from './pages/Home';
 import AuctionPage from './pages/AuctionPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
-
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path="login" element={<LoginModalWrapper />} />
           <Route path="register" element={<RegisterModalWrapper />} />
           <Route path="forgot_password" element={<ForgotPasswordWrapper />} />
-          <Route path="leilao/1" element={<AuctionPage />} />
+          <Route path="leilao/:id" element={<AuctionPage />} />
         </Routes>
       </Router>
     </>
